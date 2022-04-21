394da9a5f99c64e0583ddceb5d9ce10a6ac3bac9
Ben White <ben@62design.co.uk>
2022-04-21 14:15:33 +0100

    add delauny and voronoi graph libs from d3

--- src/draw.js
+++ src/draw.js
@@ -1,20 +1,36 @@
-const draw = ({ context: ctx, element: canvas }, { palette }) => {
 
-  ctx.fillStyle = palette.primary
+import { Delaunay } from 'd3-delaunay'
+import { displacementFromImage, scatterPoints, circle } from './utils'
+
+const draw = async ({ context: ctx, element: canvas }, { palette }) => {
+
+  const dispPoints = await displacementFromImage('./dist-maps/torus2.png', canvas.width, canvas.height)
+
+  const calcDist = (coords, exponent=3.3) => 
+    Math.pow(dispPoints[Math.floor(coords[0]) + Math.floor(coords[1]) * canvas.width], exponent)
+
+  const points = scatterPoints({shape: [canvas.width, canvas.height], distanceFunction: calcDist}) // must use fxhash RNG
+  const delaunay = Delaunay.from(points)
+  const voronoi = delaunay.voronoi([.5, .5, canvas.width - .5, canvas.height - .5])
+
+  
+  ctx.fillStyle = palette.tertiary
   ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-  ctx.strokeStyle = palette.tertiary
-
-  let pos = [500, 500]
-  ctx.moveTo(...pos)
-  ctx.lineTo(200, 200)
-  ctx.lineTo(400, 300) // store 'lastPos' in loop
-  ctx.stroke()
   ctx.beginPath()
-  ctx.lineWidth = 1 + 5 * Math.random()
-  ctx.moveTo(400, 300)
-  ctx.lineTo(100, 400)
+  voronoi.delaunay.hull = [[-1000,-1000],[1000,1000]]
+  voronoi.render(ctx)
+  ctx.strokeStyle = palette.primary
   ctx.stroke()
+
+  ctx.beginPath()
+  delaunay.render(ctx)
+  ctx.strokeStyle = palette.quaternary
+  ctx.stroke()
+
+  ctx.fillStyle = palette.secondary
+  for (let i = 0; i < points.length; i++) {
+    circle(ctx, points[i][0], points[i][1], 2)
+  }
 
 }
 
